646aa2aafb72bb6facec7330568b393eb1fc2d5b
banzzak-yl <138079411+banzzak-yl@users.noreply.github.com>
2023-07-03 13:25:27 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#pragma warning (disable: 4996)
+
+int main()
+{
+	int num1, num2;
+	double result;
+
+	scanf("%d %d", &num1, &num2);
+
+	result = (double)num1 / (double)num2;
+	printf("%.9f", result);
+
+    return 0;
+}
