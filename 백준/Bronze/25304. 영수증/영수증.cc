36482debe522899965d20f8352a3511fc45fd6fd
banzzak-yl <138079411+banzzak-yl@users.noreply.github.com>
2023-07-31 17:06:09 +0900

    [Bronze V] Title: 영수증, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/25304. 영수증/영수증.cc
+++ 백준/Bronze/25304. 영수증/영수증.cc
@@ -1,20 +1,23 @@
 #include <stdio.h>
-#pragma warning(disable:4996)
+
 int main()
 {
-	int sum, num, i, won, num1, total=0;
-	scanf("%d", &sum);
-	scanf("%d", &num);
+    int total1;
+    int total2;
+    total2=0;
+    int number;
+    int things;
+    int howmany;
+    scanf("%d",&total1);
+    scanf("%d",&number);
+    for(int i=0; i<number; i++) {
+        scanf("%d %d",&things,&howmany);
+        total2=total2+(things*howmany);
+    }
+    if(total1==total2) {
+        printf("Yes");
+    }
+    else printf("No");
 
-	for (i = 0; i < num; i++)
-	{
-		scanf("%d %d", &won, &num1);
-		total += (won * num1);
-	}
-
-	if (sum == total)
-		printf("Yes");
-	else
-		printf("No");
-	return 0;
+    return 0;
 }
