319bfc2e89e9e37ca630cf5beb571deb59bbb838
banzzak-yl <138079411+banzzak-yl@users.noreply.github.com>
2023-07-31 17:34:14 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#pragma warning(disable:4996)
+int main()
+{
+    printf("         ,r\'\"7\n");
+    printf("r`-_   ,\'  ,/\n");
+    printf(" \\. \". L_r\'\n");
+    printf("   `~\\/\n");
+    printf("      |\n");
+    printf("      |\n");
+    return 0;
+}
